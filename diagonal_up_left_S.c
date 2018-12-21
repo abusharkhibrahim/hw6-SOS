46e90c85dee19a3be8833584a6ed2590431b2dba
Ibrahim <abusharkhibrahim@gmail.com>
2018-12-21 15:42:11 -0500

    Ibrahim

---
+++ diagonal_up_left_S.c
@@ -0,0 +1,12 @@
+int diagonal_check_up_left_S(char *ptr, int n)
+{
+    int a =0;
+    if ( *(ptr -= (n+1)) == 'O')
+    {
+        if ( *(ptr -= (2*n+2)) == 'S')
+        {
+            a = a + 1;
+        }
+    }
+    return a;
+}
