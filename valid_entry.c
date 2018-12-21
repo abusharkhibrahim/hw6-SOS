2279abf5ec3ddfaa9c2ac6defcb7cdc5068cad05
Ibrahim <abusharkhibrahim@gmail.com>
2018-12-21 14:40:23 -0500

    Ibrahim

--- valid_entry.c
+++ valid_entry.c
@@ -1,13 +1,19 @@
 int valid_entry(int row, int col, char baord[n][n])
 {
-    if ( board[row][col] == ' ' )
+    if ( row <= n && col <=n)
     {
-        return 1;
+        if ( board[row][col] == ' ')
+        {
+            return 1;
+        }
+        else 
+        {
+            printf("\n The square you are trying to use is not empty. Please try again.");
+            return 0;
+        }
     }
     else 
     {
-        printf("\n The square you are trying to use is not empty. Please try again.");
-        return 0;
+        printf(" You are trying to input an invalid index. Please try again.");
     }
-
 }
