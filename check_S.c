3d49b3acd0f24376f1e287693a8d95c4777a2e7a
Ibrahim <abusharkhibrahim@gmail.com>
2018-12-21 15:28:05 -0500

    Ibrahim

--- check_S.c
+++ check_S.c
@@ -5,40 +5,42 @@ int check_S( char *ptr, int row, int col, int n)
 
     if ( (row +3) <= n)
     {
-        a = a + vertical_check_down_S(ptr);
+        a = a + vertical_check_down_S(ptr,n);
 
         if ( (col+3) <=n)
         {
-            a = a + diagonal_check_down_right_S(ptr);
+            a = a + diagonal_check_down_right_S(ptr,n);
         }
         if  ((col-3) >= 0)
         {
-            a = a + diagonal_check_down_left_S(ptr);
+            a = a + diagonal_check_down_left_S(ptr,n);
         }
     }
 
     if ( (row - 3) >= 0)
     {
-        a = a + vertical_check_up_S(ptr);
+        a = a + vertical_check_up_S(ptr,n);
 
         if ( (col+3) <= n)
         {
-            a = a + diagonal_check_up_left_S(ptr);
+            a = a + diagonal_check_up_left_S(ptr,n);
         }
         if ( (col-3) >= 0)
         {
-            a = a + diagonal_check_up_left_S(ptr);
+            a = a + diagonal_check_up_left_S(ptr,n);
         }
     }
     
     if ( (row - 3) >= 0)
     {
-        a = a + horizontal_check_left_S(ptr);
+        a = a + horizontal_check_left_S(ptr,n);
     }
 
     if ( (row + 3) <= n)
     {
-        a = a + horizontal_check_right_S(ptr);
+        a = a + horizontal_check_right_S(ptr,n);
     }
 
+
+    return a;
 }
